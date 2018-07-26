f45423da108780c2ed8479cc3dace35bcef8a257
shimaa <shimaa.elbadry54@gmail.com>
2018-07-26 20:39:59 +0200

    last elly ablha was good

--- index.js
+++ index.js
@@ -228,7 +228,30 @@ function greetingwithQuickReply(sender) {
                         }
                         }
                     };
-        callSendAPI(messageData);         
+                    request({
+                        uri: 'https://graph.facebook.com/v2.6/me/messages',
+                        qs: {
+                            access_token: config.FB_PAGE_TOKEN
+                        },
+                        method: 'POST',
+                        json: messageData
+                
+                    }, function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            var recipientId = body.recipient_id;
+                            var messageId = body.message_id;
+                
+                            if (messageId) {
+                                console.log("Successfully sent message with id %s to recipient %s",
+                                    messageId, recipientId);
+                            } else {
+                                console.log("Successfully called Send API for recipient %s",
+                                    recipientId);
+                            }
+                        } else {
+                            console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+                        }
+                });        
     }
 
 function goodmorningUserText(userId) {
