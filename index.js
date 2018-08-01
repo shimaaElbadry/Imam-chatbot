c609f9aefcb4e0945a82eed657c967f7bab6ac9a
shimaa <shimaa.elbadry54@gmail.com>
2018-08-01 11:23:44 +0200

    last was perfect with DB & 3 corrected levels

--- index.js
+++ index.js
@@ -369,7 +369,7 @@ function whatIsHadith(userId) {
 }
 
 function handleEcho(messageId, appId, metadata) {
-	// Just logging message echoes to console
+	//Just logging message echoes to console
 	console.log("Received echo for message %s and app %d with metadata %s", messageId, appId, metadata);
 }
 function handleMessage(message, sender) {
