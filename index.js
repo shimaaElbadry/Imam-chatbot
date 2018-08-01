7e3e0c02479b0f77d8201883d2bf256b3717296e
shimaa <shimaa.elbadry54@gmail.com>
2018-08-01 12:39:54 +0200

    not sure

--- index.js
+++ index.js
@@ -624,7 +624,27 @@ function sendQuickReply(senderID) {
     })
 }
 
+function receivedPostback(event) {
+	var senderID = event.sender.id;
+	var recipientID = event.recipient.id;
+	var timeOfPostback = event.timestamp;
 
+	// The 'payload' param is a developer-defined field which is set in a postback 
+	// button for Structured Messages. 
+	var payload = event.postback.payload;
+
+	switch (payload) {
+		default:
+			//unindentified payload
+			sendTextMessage(senderID, "I'm not sure what you want. Can you be more specific?");
+			break;
+
+	}
+
+	console.log("Received postback for user %d and page %d with payload '%s' " +
+		"at %d", senderID, recipientID, payload, timeOfPostback);
+
+}
 
 
 
