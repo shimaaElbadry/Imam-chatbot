caf00ab24ae19dce7c3d1d082fc1e796804facaf
shimaaElbadry <36812445+shimaaElbadry@users.noreply.github.com>
2018-08-28 15:11:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 'use strict'
 
 module.exports = {
-    FB_PAGE_TOKEN: 'EAAb834Slu54BAMRUux634ZBDb3109PIxPlnZA1oZAbRGaglrd6z6b1ownes7cC39gjZBNyK7T0uD0u3bStvsIhHEID4ZBN4cnBuz9seZABCraGQZB7GnvMcgC2SLLGyZBKyo0WKCs9Xla5O8yCn5CXOpueLs80XPcVKAXsbEVWUgjjpAceHTHftV',
-    FB_VERIFY_TOKEN: 'imam',
-    FB_APP_SECRET: '0062f3463fe21ba94cfed9890434fa03',
-    SERVER_URL: "https://imam-ub.herokuapp.com/",
+    FB_PAGE_TOKEN: '',
+    FB_VERIFY_TOKEN: '',
+    FB_APP_SECRET: '',
+    SERVER_URL: "",
     PG_CONFIG:{
-        user: 'khgyfbsttnwtew',
-        database: 'dc6kogm36kto0g',
-        password: '584cf7c96acc62dfe98af421eb0998926e1113cf59b4a54f63376fab348b1a03',
-        host: 'ec2-54-197-253-122.compute-1.amazonaws.com',
-        port: 5432,
-        max: 10,
-        idleTimeoutMillis: 30000,
+        user: '',
+        database: '',
+        password: '',
+        host: '',
+        port:,
+        max: ,
+        idleTimeoutMillis:,
     }
 };
